bea77ecbc2579d060b592c76a6c00d3600bdd610
Marwan <saadmarwan16@gmail.com>
2024-11-08 21:40:29 +0300

    Reduced bundle size 2

--- src/app/components/BackgroundWrapper.tsx
+++ src/app/components/BackgroundWrapper.tsx
@@ -8,20 +8,23 @@ const BackgroundWrapper: FunctionComponent<PropsWithChildren> = ({
 	children,
 }) => {
 	return (
-		<AuroraBackground className='h-full'>
-			<motion.div
-				initial={{ opacity: 0.0, y: 40 }}
-				whileInView={{ opacity: 1, y: 0 }}
-				transition={{
-					delay: 0.3,
-					duration: 0.8,
-					ease: 'easeInOut',
-				}}
-				className='relative flex w-full justify-center'
-			>
-				{children}
-			</motion.div>
-		</AuroraBackground>
+		<>
+			<AuroraBackground className='hidden h-full lg:block'>
+				<motion.div
+					initial={{ opacity: 0.0, y: 40 }}
+					whileInView={{ opacity: 1, y: 0 }}
+					transition={{
+						delay: 0.3,
+						duration: 0.8,
+						ease: 'easeInOut',
+					}}
+					className='relative flex w-full justify-center'
+				>
+					{children}
+				</motion.div>
+			</AuroraBackground>
+			<div className='block lg:hidden min-h-full'>{children}</div>
+		</>
 	);
 };
 
