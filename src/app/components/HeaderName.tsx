f4058d161352b4d9964fcd0a1b3ff93c1cefec06
Marwan <saadmarwan16@gmail.com>
2024-11-17 20:17:53 +0300

    Provisionally ompleted the project 2

--- src/app/components/HeaderName.tsx
+++ src/app/components/HeaderName.tsx
@@ -3,9 +3,7 @@ import { FunctionComponent } from 'react';
 interface HeaderNameProps {}
 
 const HeaderName: FunctionComponent<HeaderNameProps> = () => {
-	return (
-		<h2 className='text-3xl font-semibold md:text-4xl'>ADEBAYO</h2>
-	);
+	return <h2 className='text-3xl font-semibold md:text-4xl'>ADEBAYO</h2>;
 };
 
 export default HeaderName;
